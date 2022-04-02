c6271d8c60bab56140868faaefb0eb9ad6b0d725
hasanmuzak <hasanmuzak@yandex.com>
2022-04-02 12:01:40 +0300

    wer

--- static/netlify/functions/redirect.js
+++ static/netlify/functions/redirect.js
@@ -2,7 +2,7 @@ exports.handler = async (event, context) => {
     return {
       statusCode: 301,
       headers: {
-        Location: '/redirect/' + event.queryStringParameters.ltcid + '/'
+        Location: 'https://www.google.com.tr/'
       }
     }
   }
